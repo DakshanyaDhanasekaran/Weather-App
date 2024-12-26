18133825d1165107c4c8f90fbad320dff5ea8fc5
DakshanyaDhanasekaran <dakshanyadhanasekaran2@gmail.com>
2024-12-26 17:20:53 +0530

    config fixes...

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Enables static site generation,
-  assetPrefix: "/DakshanyaDhanasekaran/",
-  basePath: "/DakshanyaDhanasekaran",
-  trailingSlash: true
+  output: "export", // Enables static site generation
+  assetPrefix: "/Weather-App/",
+  basePath: "/Weather-App",
+  trailingSlash: true, // Adds a slash at the end of every route
 };
 
 export default nextConfig;
