0873cb0d4bb11057e3bd4f98b766a4c07bff6748
DakshanyaDhanasekaran <dakshanyadhanasekaran2@gmail.com>
2024-12-26 07:07:46 +0530

    fixes....

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // Enables static site generation,
   assetPrefix: "/DakshanyaDhanasekaran/",
-  basePath: "/DakshanyaDhanasekaran"
+  basePath: "/DakshanyaDhanasekaran",
+  trailingSlash: true
 };
 
 export default nextConfig;
